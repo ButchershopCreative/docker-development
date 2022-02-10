f40032250947dd7ed8f890f9573601f6ca400382
jesusmata <jampzo@gmail.com>
2022-02-10 00:53:10 -0400

    adjustments

--- Compilaciones/PHP/8.1.2-fqm/custom_php.sh
+++ Compilaciones/PHP/8.1.2-fqm/custom_php.sh
@@ -3,8 +3,9 @@
 case $1 in
   '1')
     #create a new php.ini
-    mv /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
+    cp -rf /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
     rm -rf /usr/local/etc/php/php.ini-production
+    cp -rf /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini /usr/local/etc/php/docker-php-ext-xdebug.ini-development
     
     #set changes to the php.ini
     sed -i 's!;html_errors = On!html_errors = On!' /usr/local/etc/php/php.ini 2>&1
@@ -15,32 +16,35 @@ case $1 in
     sed -i 's!;memory_limit = 128M!memory_limit = 2048M!' /usr/local/etc/php/php.ini 2>&1
     sed -i 's!;sendmail_path =!sendmail_path=/usr/sbin/sendmail -t -i!' /usr/local/etc/php/php.ini 2>&1
     echo "End php.ini customization..."
-
-    #echo "#[Customized hosts]" >> /etc/hosts
-    #echo "127.0.0.1 localhost" >> /etc/hosts
-    #echo "127.0.0.1 localhost.localdomain" >> /etc/hosts
-    #echo "End docker hosts customization..."
   ;;
   '2')
+    cp -rf /usr/local/etc/php/docker-php-ext-xdebug.ini-development /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    #set changes for xdebug customization
     echo "xdebug.mode=develop,debug,coverage,gcstats" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini 2>&1
     echo "xdebug.client_host=host.docker.internal" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini 2>&1 
     echo "xdebug.start_with_request=yes" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini 2>&1
     echo "xdebug.log=/var/www/xdebug.log" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini 2>&1
     echo "xdebug.idekey=vsc" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini 2>&1
     echo "xdebug.client_port=9003" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini 2>&1
+
+    #restarting php service on container
     kill -USR2 1
     echo "End php.ini - xdebug customization..."
 
+    #setting permissions to the entire dev environment
     cd /var/www/html/
     chown -R 1000:1000 * -R 
     echo "Permissions has been applied..."
   ;;
   'permissions')
+    # in case new changes on the dev files and if get any file protection.
+    #setting permissions to the entire dev environment
     cd /var/www/html/
     chown -R 1000:1000 * -R 
     echo "Permissions has been applied..."
   ;;
   *)
+    # just a touch to wake you up....
     echo "Hi there....!!!"
   ;;
 esac
